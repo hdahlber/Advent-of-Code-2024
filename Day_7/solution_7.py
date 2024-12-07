6070522fed0c92ad0fc8dd7258b545775ed62e27
hdahlber <hdahlber@abo.fi>
2024-12-07 19:51:18 +0200

    day 7.1 done

---
+++ Day_7/solution_7.py
@@ -0,0 +1,41 @@
+from itertools import product
+
+
+def part_1(filename, possible):
+    with open(filename, "r") as file:
+        summer = 0
+        lines = file.read().splitlines()
+        for line in lines:
+            ans, rest = line.split(":")
+            ans = int(ans)
+            values = list(map(int, rest.split()))
+            #print(values)
+
+            def check_if_ok(ops):
+                number = int(values[0])
+                for i in range(1, len(values)):
+
+                    if ops[i - 1] == "+":
+                        number += values[i]
+                    else:
+                        number *= values[i]
+
+                return number
+
+            for ops in product(possible, repeat=len(values) - 1):
+                if check_if_ok(ops) == ans:
+
+                    summer += ans
+                    break
+
+    return summer
+
+
+def main():
+    result = part_1("input.txt", (["+", "*"]))
+    print(f"Part 1 results: {result}")
+    #print(f"Part 2 results: {result2}")
+
+
+if __name__ == "__main__":
+    main()
