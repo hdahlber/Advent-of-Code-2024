27a2a42c25fe6b7924f453c780117eee23999dc6
hdahlber <hdahlber@abo.fi>
2024-12-12 19:51:26 +0200

    day 12.2 done, worst code ever

--- Day_12/solution_12.py
+++ Day_12/solution_12.py
@@ -10,6 +10,7 @@ def read_in_file(filename):
 
     return grid_list
 
+
 def part_1(data):
     directions = {
         "up": (-1, 0),
@@ -22,13 +23,14 @@ def part_1(data):
     col_len = len(data[0])
     row_len = len(data)
     summer = 0
+    summer2 = 0
 
     def calculate_lawn_size(row_index, col_index):
         stack = [(row_index, col_index)]
         lawn_char = data[row_index][col_index]
         lawn_size = 0
         sides = 0
-
+        edges = set()
         while stack:
             r, c = stack.pop()
             if (r, c) in visited:
@@ -47,28 +49,69 @@ def part_1(data):
                         if (new_row, new_col) not in visited:
                             stack.append((new_row, new_col))
                         local_sides -= 1
+                    else:
+                        edges.add(((r, c), direction))
+                else:
+                    edges.add(((r, c), direction))
 
             sides += local_sides
-
-        return lawn_char, lawn_size, sides
+        part_2_ans = part_2(edges)
+        return lawn_char, lawn_size, sides, part_2_ans
 
     for row_index, row in enumerate(data):
         for col_index, value in enumerate(row):
             if (row_index, col_index) not in visited:
-                lawn_char, lawn_size, sides = calculate_lawn_size(row_index, col_index)
+                lawn_char, lawn_size, sides, part_2_ans = calculate_lawn_size(row_index, col_index)
                 summer += lawn_size * sides
+                summer2 += lawn_size * part_2_ans
+    return summer, summer2
 
-    return summer
+
+def part_2(edges):
+    rows_set = set()
+    cols_set = set()
+    summer3 = 0
+    sorted_edges = sorted(edges, key=lambda x: (x[0][0], x[0][1]))
+    sorted_edges_columns = sorted(edges, key=lambda x: (x[0][1], x[0][0]))
+
+    for edge in sorted_edges:
+        ((row, column), direction) = edge
+        if direction == "up":
+            #print((row, column), direction)
+            rows_set.add(edge)
+            if ((row, column - 1), "up") not in rows_set:
+                #print("aadddd 1")
+                summer3 += 1
+        if direction == "down":
+            #print((row, column), direction)
+            rows_set.add(edge)
+            if ((row, column - 1), "down") not in rows_set:
+                #print("aadddd 2")
+                summer3 += 1
+
+    #print("-----------")
+    for edge in sorted_edges_columns:
+        ((row, column), direction) = edge
+        if direction == "left":
+            #print((row, column), direction)
+            cols_set.add(edge)
+            if ((row - 1, column), "left") not in cols_set:
+                summer3 += 1
+        if direction == "right":
+            cols_set.add(edge)
+            if ((row - 1, column), "right") not in cols_set:
+                summer3 += 1
+
+    #print("---1111111111--")
+    print(summer3)
+    return summer3
 
 
 def main():
     grid_list = read_in_file("input.txt")
-    result = part_1(grid_list)
+    result, result2 = part_1(grid_list)
     print(f"Part 1 results: {result}")
-
-
-
-
+    print(f"Part 2 results: {result2}")
 
 
 if __name__ == "__main__":
