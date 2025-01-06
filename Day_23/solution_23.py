c9e0691a78f8c935b60c673cddc5072493d2d16c
hdahlber <hdahlber@abo.fi>
2025-01-06 12:06:18 +0200

    reformat day 23

--- Day_23/solution_23.py
+++ Day_23/solution_23.py
@@ -5,6 +5,8 @@ def read_in_file(filename):
     with open(filename, "r") as file:
         read = file.read().strip().split("\n")
     return read
+
+
 def create_graph(edges):
     G = nx.Graph()
     for edge in edges:
@@ -15,18 +17,22 @@ def create_graph(edges):
 
     return G
 
+
 def part_1(graph):
     cliques = [clique for clique in nx.enumerate_all_cliques(graph) if len(clique) == 3]
     #print(len(cliques))
-    filtered_cliques = [tuple(sorted(clique)) for clique in cliques if any(graph.nodes[node]["value"] for node in clique)]
+    filtered_cliques = [tuple(sorted(clique)) for clique in cliques if
+                        any(graph.nodes[node]["value"] for node in clique)]
     #print(len(filtered_cliques))
     return len(filtered_cliques)
+
 
 def part_2(graph):
     cliques = list(nx.find_cliques(graph))
     biggest_clique = sorted(max(cliques, key=len))
     result = ",".join(biggest_clique)
     return result
+
 
 def main():
     file_edges = read_in_file("input.txt")
@@ -35,6 +41,7 @@ def main():
     print(f"Part 1 results: {result}")
     result2 = part_2(graph)
     print(f"Part 2 results: {result2}")
+
 
 if __name__ == "__main__":
     main()
